8c0e702576ec3643bc8b4c74f20daa0864016bf0
jose99segura <jose99segura@gmail.com>
2024-05-15 11:20:15 +0200

    Playing with MVC

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -15,6 +15,7 @@ namespace Portafolio.Controllers
 
         public IActionResult Index()
         {
+            ViewBag.Nombre = "Jose Luis Segura Navarro";
             return View();
         }
 

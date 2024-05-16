f69fd394fad2bc7eefe3b0cec667c3b74d75e4aa
jose99segura <jose99segura@gmail.com>
2024-05-16 11:58:39 +0200

    Portfolio simple finished

---
+++ Portafolio/Models/ContactoViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Portafolio.Models
+{
+    public class ContactoViewModel
+    {
+        public String Nombre { get; set; }
+        public string Email { get; set; }
+        public string Mensaje { get; set; }
+    }
+}
